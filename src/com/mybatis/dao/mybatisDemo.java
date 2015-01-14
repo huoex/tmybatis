f1016d4e6dc59eb6368bdb55546a41527f5a2b79
huoex <huoex0628@gmail.com>
2015-01-14 09:26:06 +0800

    Initial commit
    Signed-off-by: huoex <huoex0628@gmail.com>

---
+++ src/com/mybatis/dao/mybatisDemo.java
@@ -0,0 +1,12 @@
+package com.mybatis.dao;
+
+public class mybatisDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TcontactosDAO tdao = new TcontactosDAO();
+		tdao.selectAll();
+
+	}
+
+}

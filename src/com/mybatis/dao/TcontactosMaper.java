05cf4007735f539c8de81d2afd13706da05663d5
huoex <huoex0628@gmail.com>
2015-02-04 15:28:42 +0800

    testmaper.xml
    Signed-off-by: huoex <huoex0628@gmail.com>

--- src/com/mybatis/dao/TcontactosMaper.java
+++ src/com/mybatis/dao/TcontactosMaper.java
@@ -1,10 +1,12 @@
 package com.mybatis.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Select;
 
 import com.mybatis.models.Tcontactos;
 
 public interface TcontactosMaper {
-	@Select("SELECT * FROM contactos WHERE id = #{id}")
-	Tcontactos selectTcon(int id);
+//	@Select("SELECT * FROM contactos WHERE id = #{id}")
+	List<Tcontactos> selectTcon(int id);
 }
--- src/com/mybatis/dao/mybatisDemo.java
+++ src/com/mybatis/dao/mybatisDemo.java
@@ -3,21 +3,27 @@ package com.mybatis.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import com.mybatis.models.Tcontactos;
+
 public class mybatisDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//XML方式
-		TcontactosDAO tdao = new TcontactosDAO();
-		tdao.selectAll();
-		
+//		TcontactosDAO tdao = new TcontactosDAO();
+//		tdao.selectAll();
+//		
 		//注解方式
 		SqlSessionFactory sqlSessionFactory = MyBatisConnecctionFactory.getSqlSessionFactory();
 		//这步必须做，不然会报错
-		sqlSessionFactory.getConfiguration().addMapper(TcontactosMaper.class);
 		SqlSession session = sqlSessionFactory.openSession();
+//		session.selectOne("abc.getById",1);
+//		session.selectMap("abc.getById","1");
+//		sqlSessionFactory.getConfiguration().addMapper(TcontactosMaper.class);
+//		session.getMapper(Tcontactos.class);
 		TcontactosMaper tcontactosMaper = session.getMapper(TcontactosMaper.class);
 		tcontactosMaper.selectTcon(1);
+		System.out.println();
 
 	}
 
